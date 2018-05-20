ce0f7f4a498e40251c94b2fab2d7aca510e68615
yiochen <chen.yiou1992@gmail.com>
2018-05-20 14:21:38 -0700

    redirect to project demo

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from "react";
+import trim from "lodash/trim";
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+class NotFoundPage extends React.Component {
+  state = {
+    possibleProjectName: ""
+  };
+  componentDidMount() {
+    const possibleProjectName = trim(window.location.pathname, "/");
+    this.setState({
+      possibleProjectName
+    });
+  }
+  render() {
+    const { possibleProjectName } = this.state;
+    return (
+      <div>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        {possibleProjectName && (
+          <p>
+            Are you looking for my project called {possibleProjectName}, you can
+            head to{" "}
+            <a
+              target="_blank"
+              href={`https://yiochen.github.io/${possibleProjectName}`}
+            >
+              here
+            </a>
+          </p>
+        )}
+      </div>
+    );
+  }
+}
 
-export default NotFoundPage
+export default NotFoundPage;
